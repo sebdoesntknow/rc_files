96542ec134b94454ba0fcec71a30922cb9130eaf
Sebastian <seba.grillo@gmail.com>
2016-07-26 21:57:00 -0300

    Emacs eslint, flycheck and JSX config added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,25 @@
-;;'(exec-path (quote ("/usr/bin" "/bin" "/usr/sbin" "/sbin")))
-;;'(fci-rule-color "#f1c40f")
-
 ;; Initializations
+;; JSX/ESLINT setup from http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+(add-to-list 'auto-mode-alist 
+             '("\\.jsx$" . web-mode))
 
-(ido-mode t)
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+    "Hooks for Web mode. Adjust indents"
+      ;;; http://web-mode.org/
+        (setq web-mode-markup-indent-offset 2)
+          (setq web-mode-css-indent-offset 2)
+            (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+             (if (equal web-mode-content-type "jsx")
+                   (let ((web-mode-enable-part-face nil))
+                           ad-do-it)
+                       ad-do-it))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -16,12 +32,16 @@
     ("ee17bcdaa3ce2c5b287dff4d4c1da13f45bfa2f1aa9e11ff0bbbebb2e6be79c5" "754b61c05f9ec4d0462aee7c73e350a56f3d8f88610e67b1fe7b5efc646dc0ce" default)))
  '(custom-theme-directory "~/.emacs.d/themes")
  '(custom-theme-load-path (quote ("~/.emacs.d/themes" custom-theme-directory t)))
+ '(flycheck-disabled-checkers (quote (javascript-jshint json-jsonlist)))
+ '(flycheck-mode-hook (quote (flycheck-mode-set-explicitly)))
+ '(global-flycheck-mode t)
+ '(ido-mode (quote both) nil (ido))
  '(menu-bar-mode nil)
  '(package-archives
    (quote
     (("melpa" . "http://melpa.org/packages/")
      ("gnu" . "http://elpa.gnu.org/packages/"))))
- '(package-enable-at-startup nil)
+ '(package-enable-at-startup t)
  '(package-user-dir "~/.emacs.d/elpa")
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
